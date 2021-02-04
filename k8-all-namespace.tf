265cae71cb3e038affbd1a27e983c13754bb6248
vasanthonline <vasanthgonline@gmail.com>
2021-02-04 15:53:20 +0530

    Create k8-all-namespace.tf

---
+++ k8-all-namespace.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+data "kubernetes_all_namespaces" "allns" {}
+
+output "all-ns" {
+  value = data.kubernetes_all_namespaces.allns.namespaces
+}
+
+output "ns-present" {
+  value = contains(data.kubernetes_all_namespaces.allns.namespaces, "kube-system")
+}
